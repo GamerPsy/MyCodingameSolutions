7cb2e4e3acd3a440fcc6e4740b52a6bcb0e14608
Rémy DUCRY <ducry.remy@gmail.com>
2022-12-15 09:05:27 +0100

    ajout solution There is no spoon

--- Puzzles_Moyens/ThereIsNoSpoon_episode_1.php
+++ Puzzles_Moyens/ThereIsNoSpoon_episode_1.php
@@ -3,19 +3,41 @@
  * Don't let the machines win. You are humanity's last hope...
  **/
 
-// $width: the number of cells on the X axis
-fscanf(STDIN, "%d", $width);
-// $height: the number of cells on the Y axis
-fscanf(STDIN, "%d", $height);
-for ($i = 0; $i < $height; $i++)
+// $largeur: the number of cells on the X axis
+// $hauteur: the number of cells on the Y axis
+fscanf(STDIN, "%d", $largeur);
+fscanf(STDIN, "%d", $hauteur);
+for ($i = 0; $i < $hauteur; $i++)
 {
-    $line = stream_get_line(STDIN, 31 + 1, "\n");// width characters, each either 0 or .
+    $grille[] = str_split(stream_get_line(STDIN, 31 + 1, "\n"));// largeur characters, each either 0 or .
 }
 
-// Write an action using echo(). DON'T FORGET THE TRAILING \n
-// To debug: error_log(var_export($var, true)); (equivalent to var_dump)
-
-
-// Three coordinates: a node, its right neighbor, its bottom neighbor
-echo("0 0 1 0 0 1\n");
-?>
+// Pour chaque nœud d'alimentation de la grille
+for ($y = 0; $y < $hauteur; $y++) {
+    for ($x = 0; $x < $largeur; $x++) {
+      if ($grille[$y][$x] == '0') {
+        // Trouver le premier nœud situé sur sa droite
+        $noeud_a_droite = [-1, -1];
+        for ($xx = $x + 1; $xx < $largeur; $xx++) {
+          if ($grille[$y][$xx] == '0') {
+            $noeud_a_droite = [$xx, $y];
+            break;
+          }
+        }
+  
+        // Trouver le premier nœud situé sous lui
+        $noeud_en_bas = [-1, -1];
+        for ($yy = $y + 1; $yy < $hauteur; $yy++) {
+          if ($grille[$yy][$x] == '0') {
+            $noeud_en_bas = [$x, $yy];
+            break;
+          }
+        }
+  
+        // Afficher les coordonnées du nœud, du voisin de droite, et du voisin situé en dessous
+        echo "$x $y " . implode(' ', $noeud_a_droite) . ' ' . implode(' ', $noeud_en_bas) . "\n";
+      }
+    }
+  }
+  
+  
